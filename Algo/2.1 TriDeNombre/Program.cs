200791120d1321d82e5144d1d573d5e9d8b4749e
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2022-10-26 09:35:12 +0200

    Change today

--- Algo/2.1 TriDeNombre/Program.cs
+++ Algo/2.1 TriDeNombre/Program.cs
@@ -10,45 +10,27 @@ b = int.Parse(Console.ReadLine());
 Console.WriteLine("Veuillez-saisir un nombre c");
 c = int.Parse(Console.ReadLine());
 
-if (a > b)
+if (a < b)
 {
     int temp = a;
     a = b;
     b = temp;
-    if (a > c)// a a pris la valeur de b
-    {
-        temp = a;
-        a = c;
-        c = temp;
-    }
-    if (b > a)
-    {
-        temp = c;
-        c = b;
-        b = temp;
-
-    }
-    else if (a > c)
-    {
-
-        temp = a;
-        a = c;
-        c = temp;
-        if (b > c)
-        {
-            temp = c;
-            c = b;
-            b = temp;
-        }
-    }
-    else if (b > c)
-    {
-        temp = b;
-        b = c;
-        c = temp;
-    }
 }
-    Console.WriteLine(a + " " + b + " " + c);
+
+else if (b < c)
+{
+    int temp = b;
+    b = c;
+    c = temp; 
+}
+else if (a < c)
+{
+    int temp = c;
+    c = a;
+    a = temp;
+}
+Console.WriteLine(a + " " + b + " " + c);
+
 
 
 
