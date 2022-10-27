b10800296e14eb74cb1db5f01a43651c1b96c62a
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2022-10-27 16:11:14 +0200

    save 27 10 2022

--- Algo/2.3 TriNombre++/Program.cs
+++ Algo/2.3 TriNombre++/Program.cs
@@ -1,5 +1,65 @@
-﻿int a;
-int b;
-int c;
+﻿double a;
+double b;
+double c;
 
 Console.WriteLine("Veuillez saisir un nombre entier svp");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Veullez sasir un second nombre entier svp");
+b = double.Parse(Console.ReadLine());  
+Console.WriteLine("Veuillez saisir un troisieme nombre entier svp");
+c = double.Parse(Console.ReadLine());
+
+
+double min, med, max;
+
+
+if (a < b)
+{
+    if (b < c)
+    {
+        min = a;
+        med = b;
+        max = c;
+    } else
+    {
+        if (a < c)
+        {
+            min = a;
+            med = c;
+            max = b;
+        } else
+        {
+            min = c;
+            med = a;
+            max = b;
+        }
+    }
+} else
+{
+    if (a < c)
+    {
+        min = b;
+        med = a;
+        max = c;
+    }
+    else
+    {
+        if (b < c)
+        {
+            min = b;
+            med = c;
+            max = a;
+        }
+        else
+        {
+            min = c;
+            med = b;
+            max = a;
+        }
+    }
+}
+a = min;
+b = med;
+c = max;
+
+Console.WriteLine(String.Format("{0}<={1}<={2}", a, b, c));
