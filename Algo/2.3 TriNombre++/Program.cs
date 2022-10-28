5d7d451c84e92946267a3164b6bce1253306b67d
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2022-10-28 11:20:56 +0200

    send 28 10 2022

--- Algo/2.3 TriNombre++/Program.cs
+++ Algo/2.3 TriNombre++/Program.cs
@@ -1,4 +1,35 @@
-﻿double a;
+﻿/*double[] numbers = new double[3];
+
+Console.WriteLine("Veuillez saisir un nombre entier svp");
+numbers[0] = double.Parse(Console.ReadLine());
+Console.WriteLine("Veullez sasir un second nombre entier svp");
+numbers[1] = double.Parse(Console.ReadLine());  
+Console.WriteLine("Veuillez saisir un troisieme nombre entier svp");
+numbers[2] = double.Parse(Console.ReadLine());
+numbers = trierTableau(numbers);
+foreach(double number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+static double[] trierTableau(double[] tableauATrier)
+{
+    double tmp;
+    for (int i = 0; i < tableauATrier.Length - 1; i++)
+    {
+        for (int j = i; j < tableauATrier.Length; j++)
+        {
+            if (tableauATrier[j] < tableauATrier[i])
+            {
+                tmp = tableauATrier[j];
+                tableauATrier[j] = tableauATrier[i];
+                tableauATrier[i] = tmp;
+            }
+        }
+    }
+    return tableauATrier;
+}*/
+double a;
 double b;
 double c;
 
@@ -8,6 +39,10 @@ Console.WriteLine("Veullez sasir un second nombre entier svp");
 b = double.Parse(Console.ReadLine());  
 Console.WriteLine("Veuillez saisir un troisieme nombre entier svp");
 c = double.Parse(Console.ReadLine());
+
+
+
+
 
 
 double min, med, max;
@@ -34,7 +69,8 @@ if (a < b)
             max = b;
         }
     }
-} else
+}
+else
 {
     if (a < c)
     {
@@ -63,3 +99,5 @@ b = med;
 c = max;
 
 Console.WriteLine(String.Format("{0}<={1}<={2}", a, b, c));
+
+
---
+++ Algo/Exercice_2.4/Exercice_2.4_Annee_Bissextike/Program.cs
@@ -0,0 +1,15 @@
+﻿int a;
+Console.WriteLine("Veuillez saisir une annee svp.");
+a = int.Parse(Console.ReadLine());
+
+/*Déterminer si l'année A est bissextile. Si A n'est pas divisible par 4, l'année n'est pas bissextile.
+Si A est divisible par 4, l'année est bissextile sauf si A est divisible par 100 et pas par 400.
+Afficher le message « Bissextile » ou « Non bissextile » suivant le cas.
+*/
+if (((a % 4 == 0) && (a % 100 !=0)) || (a % 400 == 0)){
+    Console.WriteLine("L'année " + a + " est bissextile");
+}
+else
+{
+    Console.WriteLine("L'année " + a + " n'est pas bissextile");
+}
