229652441ec0b0050d82865d97002512097a71ee
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2022-11-07 15:18:26 +0100

    send today

---
+++ Algo/2.5 RechercheDesDiviseursD'UnNombre/Program.cs
@@ -0,0 +1,16 @@
+﻿
+/*Lire un nombre entier et afficher tous ses diviseurs autres que 1 et lui-même.
+ */
+/*
+ * ecrire un nombre N
+ * lire nombre N
+*/
+    
+try
+{
+Console.WriteLine("Hello, World!");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(" Saisie Incorrecte !" + ex.Message);
+ }
