4ec54ca21d01ca6e41e749c997bdca47c427c19d
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2022-11-08 15:04:38 +0100

    Save 08 11 2022

---
+++ Algo/3.1 JeuDeLaFourchette/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+L'ordinateur « choisit » aléatoirement un nombre mystère (entier compris entre 0 et 100). Le joueur essaie de le deviner. 
+Lors de chaque essai, l'ordinateur affiche la « fourchette » dans laquelle se trouve le nombre qu'il a choisi. 
+Le choix du nombre mystère sera simulé par génération d'un nombre aléatoire : N <-- RANDOM(0,100).
+Lorsque l'utilisateur a trouvé le nombre mystère, le programme affiche "Bravo vous avez trouvé en X essais"
+ */
+
+Console.WriteLine("Hello, World!");
+
+int userInput;
+int N;
+int numberOfTrials = 0;
+
+Random rnd = new Random();
