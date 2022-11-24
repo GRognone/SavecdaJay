99feb2c8cfae7d91780b31f7b4eea1dc7bb4e5fd
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2022-11-24 16:01:47 +0100

    send Today 24 11 2022

--- Algo/6.2 Palindrome/6.2 Palindrome/Program.cs
+++ Algo/6.2 Palindrome/6.2 Palindrome/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection.Emit;
 using System.Reflection.PortableExecutable;
 using System;
+using System.Reflection.Metadata;
 
 namespace _6._2_Palindrome
 {
@@ -8,25 +9,39 @@ namespace _6._2_Palindrome
     {
         static void Main(string[] args)
         {
-  /*Un palindrome est une chaîne de caractères que l'on peut lire identiquement de droite à gauche, et gauche à droite.
-  Par exemple:
-  • AA
-  • 38783
-  • LAVAL
-  • LAVAL A ETE A LAVAL
-  • ET LA MARINE VA VENIR A MALTE
-  Soit un texte d'au moins 2 caractères (à contrôler).
-  Ecrivez l'algorithme d'un programme permettant d'affirmer si cette phrase est un palindrome.
-  Si la chaîne de caractères est vide, le message 'LA CHAINE EST VIDE' sera affiché.
-  Proposez un jeu d'essai prévoyant les 3 cas suivants :
-  ▪ la phrase est vide
-  ▪ la chaîne de caractères n'est pas un palindrome
-  ▪ la chaîne de caractères est un palindrome
-  */
+            /*Un palindrome est une chaîne de caractères que l'on peut lire identiquement de droite à gauche, et gauche à droite.
+            Par exemple:
+            • AA
+            • 38783
+            • LAVAL
+            • LAVAL A ETE A LAVAL
+            • ET LA MARINE VA VENIR A MALTE
+            Soit un texte d'au moins 2 caractères (à contrôler).
+            Ecrivez l'algorithme d'un programme permettant d'affirmer si cette phrase est un palindrome.
+            Si la chaîne de caractères est vide, le message 'LA CHAINE EST VIDE' sera affiché.
+            Proposez un jeu d'essai prévoyant les 3 cas suivants :
+            ▪ la phrase est vide
+            ▪ la chaîne de caractères n'est pas un palindrome
+            ▪ la chaîne de caractères est un palindrome
+            */
 
 
+            char userInput;
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Verifions si une chaine de caractère entré par un utilisateur est vide.");
+            do
+            {
+                Console.WriteLine("Veuillez - saisir une chaine de craractere svp.");
+            }
+            while
+            {
+
+            }
+            Console.WriteLine("Veuillez - saisir une chaine de craractere svp.");
+
+            Console.WriteLine("La Phrase est vide.");
+            Console.WriteLine("la chaîne de caractères n'est pas un palindrome");
+            Console.WriteLine("la chaîne de caractères est un palindrome");
         }
     }
 }
---
+++ Algo/6.4 RechercheParDichotomie/6.4 RechercheParDichotomie/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+
+namespace _6._4_RechercheParDichotomie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*ENONCE
+            *Recherche par dichotomie d'un élément dans une table classée.
+            *Soit une table contenant des prénoms, classés par ordre alphabétique
+            *Nous désirons chercher l'indice de la case de la table où se trouve le prénom, si il s'y trouve.
+            *Pour cela, nous utiliserons la méthode de dichotomie (voir ci-dessous la méthode).
+            *Donnez l'algorithme de la procédure qui recherche, par dichotomie le numéro du prénom recherché ou zéro s'il n'y est pas.
+            *Principe de la recherche par dichotomie:
+            *Les prénoms sont classés par ordre alphabétique
+            *On connaît le nombre d'éléments de la table
+            *Algorithme:
+            *On partitionne la table en 2 sous-tables et un élément médian, et, suivant le résultat de la comparaison de l'élément médian et 
+            *du prénom recherché (plus grand, plus petit ou égal) on recommence la recherche sur une des 2 sous-tables, jusqu'à avoir 
+             */
+
+            /*PSEUDO CODE
+             
+             * DECLARATIONS
+             
+             * ecrire string userInput
+             * lire userInput
+             * declarer un tableau de string name
+             * declarer un string searchName
+             * declarer un int min
+             * declarer un int max
+             * declarer un int delta
+             * declarer un int median
+             * declarer boolean find
+              
+             * TRAITEMENT
+             *  nom <-- (agathe, berthe, chloé, cunegonde, olga, raymonde, sidonie)
+             *  min <-- 0
+             *  max <-- longueur name
+             *  searchName <-- olga
+             *  delta <-- (max - min / 2)
+             *  mediane <-- min + delta
+             *  find <-- false
+             *  find <-- name[median] egal searchName
+             *  
+             *  ecrire " Bonjour, vous êtes sur un programme de recherche de prénom par dichitomie"
+             *  TANT QUE (nom[min+delta} est different de nomAChercher et delta different de 0)
+             *  
+             *  SI searchName est alphabethiquement avant name [median]
+             *  max <-- median
+             *  
+             *  SINON
+             *  min <-- median
+             *  
+             *  FINSI
+             *  
+             *  delta <-- (max - min / 2)
+             *  mediane <-- min+delta
+             *  find <-- name[median] egal searchName
+             *  
+             *  SI !find et delta ==0
+             *      SI name[max] == searchName 
+             *          median <-- max
+             *          find <-- true
+             *      FINSI
+             *      
+             *  FINSI
+             *  FIN TANT QUE
+             * 
+             * AFFICHAGE
+             * 
+             * SI find est vrai 
+             * Ecrire "le nom a été trouvé à l'indice", median
+             * SINON
+             * Ecrire "Le nom n'est pas présent dans le tableau"
+             * FIN SI
+             * 
+             * FONCTION
+            */
+
+            string userInput;
+            userInput = Console.ReadLine();
+            string[] name = new[] { "agathe, berthe, chloé, cunegonde, olga, raymonde, sidonie" };
+            string searchName;
+            int min;
+            int max;
+            int delta;
+            int median;
+            bool find;
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
