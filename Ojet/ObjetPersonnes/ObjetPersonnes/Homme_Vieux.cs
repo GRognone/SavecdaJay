f128c355a29f596f9021a365b32225d365f8aca2
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2022-12-07 16:59:31 +0100

    Save 07 12 2022

---
+++ Ojet/ObjetPersonnes/ObjetPersonnes/Homme_Vieux.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetPersonnes
+{
+    public class Homme_Vieux : Homme
+    {
+        public override void Marcher()
+        {
+         
+        }
+    }
+}
---
+++ Ojet/ObjetPersonnes/ObjetPersonnes/Personne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetPersonnes
+{
+    public abstract class Personne
+    {
+         int age;
+        public void DanserDisco()
+        {
+        }
+        public abstract void  DanserSalon();
+        public virtual void  Marcher()
+        {
+        }
+    }  
+}
+
+
+
+
+
+
