5f61582a089d798374e2b6f44b9e2e3b1d120508
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2022-12-15 15:46:49 +0100

    send afternoon

--- Ojet/Voiture/Voiture/Program.cs
+++ Ojet/Voiture/Voiture/Program.cs
@@ -13,33 +13,39 @@
             bool retour4 = roueParDefaut.Stopper();//true
 
             Moteur moteur_parDefaut = new Moteur();
-            Moteur monMoteur = new Moteur(moteur_parDefaut);
+            Roue roue1 = new Roue();
+            Roue roue2 = new Roue();
 
-            bool retour5 = moteur_parDefaut.Demarrer();//true
-            bool retour6 = moteur_parDefaut.Eteindre();//false
-            bool retour7 = moteur_parDefaut.EntrainerRoue(maRoue,maRoue);//true
-            bool retour8 = moteur_parDefaut.ArreterRoues(maRoue, maRoue);//false
-            bool retour9 = moteur_parDefaut.ArreterRoues(maRoue, maRoue);//true
-            bool retour10 = moteur_parDefaut.EntrainerRoue(maRoue, maRoue);//false
-            bool retour11 = moteur_parDefaut.Eteindre();//true
-            bool retour12 = moteur_parDefaut.Demarrer();//false
-
-
-            Voiture voiture_parDefaut = new Voiture();
-            Voiture voiturevoiture = new Voiture(voiture_parDefaut);
-
-            bool retour13 = voiture_parDefaut.Demarrer(); //true
-            bool retour14 = voiture_parDefaut.Avancer(); //true
-            bool retour15= voiture_parDefaut.Avancer();//false
-            bool retour16 = voiture_parDefaut.CouperContact();//false
-            bool retour17 = voiture_parDefaut.Freiner();//true
-            bool retour18 = voiture_parDefaut.Freiner();//false
-            bool retour19 = voiture_parDefaut.CouperContact();//true
-            bool retour20 = voiture_parDefaut.Demarrer();//false
+            bool retour5 = moteur_parDefaut.Eteindre();//false
+            bool retour6 = moteur_parDefaut.EntrainerRoue(maRoue, maRoue);//false
+            bool retour7 = moteur_parDefaut.ArreterRoues(maRoue, maRoue);//false
+            bool retour8 = moteur_parDefaut.Demarrer();//true
+            bool retour9 = moteur_parDefaut.Demarrer();//false
+            bool retour10 = moteur_parDefaut.ArreterRoues(maRoue, maRoue);//false
+            bool retour11 = moteur_parDefaut.EntrainerRoue(maRoue, maRoue);//true
+            bool retour12 = moteur_parDefaut.EntrainerRoue(maRoue, maRoue);//false
+            bool retour13 = moteur_parDefaut.ArreterRoues(maRoue, maRoue);//true
+            bool retour14 = moteur_parDefaut.Eteindre();//true
            
 
 
-          int nc = 0;
+            Voiture voiture_parDefaut = new Voiture();
+            Voiture mavoiture = new Voiture(voiture_parDefaut);
+
+            bool retour15 = voiture_parDefaut.Demarrer(); //true
+            bool retour16 = voiture_parDefaut.Avancer(); //true
+            bool retour17= voiture_parDefaut.Avancer();//false
+            bool retour18 = voiture_parDefaut.CouperContact();//false
+            bool retour19 = voiture_parDefaut.Freiner();//true
+            bool retour20 = voiture_parDefaut.Freiner();//false
+            bool retour21 = voiture_parDefaut.CouperContact();//true
+            bool retour22 = voiture_parDefaut.CouperContact();//false
+            bool retour23 = voiture_parDefaut.Freiner();//false
+            bool retour24 = voiture_parDefaut.Avancer();//false
+
+
+
+            int nc = 0;
         }
     }
 }
