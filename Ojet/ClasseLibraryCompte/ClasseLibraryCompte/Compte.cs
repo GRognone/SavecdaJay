cb8703900d06e6243875ce1f123a3ef906795d0c
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-01-06 11:47:05 +0100

    send 06 01 2022

---
+++ Ojet/ClasseLibraryCompte/ClasseLibraryCompte/Compte.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasseLibraryCompte
+{
+    public class Compte
+    {
+        public string nomProprietaireCompte;
+        public int numeroUnique;
+        public int montantDecouvertAutorise;
+        public float soldeDuCompte;
+
+        public Compte() : this(0,"Monsieur X", 0f, 0)
+        {
+            
+        }
+
+        public Compte(int _numero, string _nom, float _solde, int _decouvertAutorise)
+        {
+            nomProprietaireCompte = _nom;
+            numeroUnique = _numero;
+            montantDecouvertAutorise = _decouvertAutorise;
+            soldeDuCompte = _solde;
+        }
+
+        public void CrediterCompte(float _montant)
+        {
+            
+        }
+
+        public bool DebiterCompte(float _motant)
+        {
+            if(soldeDuCompte <= montantDecouvertAutorise)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public bool TransfererMontantVersAutreCompte(float _montantATransferer, Compte _compteDestinataire)
+        {
+            return false;
+        }
+
+        public string ToString()
+        {
+            return "";
+        }
+
+        public bool Superieur(Compte _autreCompte)
+        {
+            return false;
+        }
+
+        public void ChangerDecouver(int nouveauDecouvert)
+        {
+            montantDecouvertAutorise = nouveauDecouvert;
+        }
+    }
+}
+
---
+++ Ojet/ClasseLibraryCompte/ConsoleAppCompte/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClasseLibraryCompte;
+
+Console.WriteLine("Hello, World!");
+Compte c = new Compte();
+
+    Compte c1 =new Compte(16985, Console.ReadLine(), 1250324.04F, -500);
+c1.ChangerDecouver(-5000);
+Console.WriteLine("");
+/*c.CrediterCompte(100);
+Console.WriteLine(c);
+bool ok = c.DebiterCompte(100000);
+Console.WriteLine(c.ToString());
+
+if (ok)
+{
+    Console.WriteLine("debit reussi");
+}
+
+
+else
+{
+    Console.WriteLine("debit pas reussi");
+}*/
