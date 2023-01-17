2c4faf420e33eca028846c7e7d07a680dd3738ae
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-01-17 16:32:00 +0100

    save 17 01 2023

--- Ojet/Fraction/ConsoleApp1TestProgrammeFraction/Program.cs
+++ Ojet/Fraction/ConsoleApp1TestProgrammeFraction/Program.cs
@@ -8,37 +8,37 @@ namespace ConsoleApp1TestProgrammeFraction
         {
             Fraction f = new Fraction();
 
-            /* Fraction f1 = new Fraction(12, 7);
-             Console.WriteLine(f1.ToString());
+            Fraction f1 = new Fraction(12, 0);
+            Console.WriteLine(f1.ToString());
 
-             Fraction f2 = new Fraction(9);
-             Console.WriteLine(f2.ToString());
+            Fraction f2 = new Fraction(9);
+            Console.WriteLine(f2.ToString());
 
-             Fraction f3 = new Fraction();
-             Console.WriteLine(f3.ToString());
+            Fraction f3 = new Fraction();
+            Console.WriteLine(f3.ToString());
 
-             Fraction f4 = new Fraction(4,7);
-             f4.Oppose();
-             Console.WriteLine(f4.ToString());
+            Fraction f4 = new Fraction(4, 7);
+            f4.Oppose();
+            Console.WriteLine(f4.ToString());
 
-             Fraction f5 = new Fraction(4,7);
-             f5.Inverse();
-             Console.WriteLine(f5.ToString());
+            Fraction f5 = new Fraction(4, 7);
+            f5.Inverse();
+            Console.WriteLine(f5.ToString());
 
-             Fraction f6 = new Fraction(11,7);
-             Console.WriteLine(f6.ToString());
-             Fraction f7 = new Fraction(5,4);
-             Console.WriteLine(f7.ToString());
-             bool estSuperieur = f6.SuperieurA(f7);
+            Fraction f6 = new Fraction(11, 7);
+            Console.WriteLine(f6.ToString());
+            Fraction f7 = new Fraction(5, 4);
+            Console.WriteLine(f7.ToString());
+            bool estSuperieur = f6.SuperieurA(f7);
 
-             Fraction f8 = new Fraction(11,7);
-             Console.WriteLine(f8.ToString());
-             Fraction f9 = new Fraction(22,14);
-             Console.WriteLine(f9.ToString());
-             bool estEgal = f8.EgalA(f9);
-            
+            Fraction f8 = new Fraction(11, 7);
+            Console.WriteLine(f8.ToString());
+            Fraction f9 = new Fraction(22, 14);
+            Console.WriteLine(f9.ToString());
+            bool estEgal = f8.EgalA(f9);
+
             Fraction f10 = new Fraction(120, -150);
-            Console.WriteLine(f10.ToDisplay()); */
+            Console.WriteLine(f10.ToDisplay()); 
 
             Fraction f11 = new Fraction(5, 2);
             Fraction f12 = new Fraction(3, 4);
@@ -60,7 +60,7 @@ namespace ConsoleApp1TestProgrammeFraction
             Fraction division = f11 / f12; // division surcharge operateur
             Console.WriteLine(division.ToDisplay());
 
-            Fraction frac = new(0,5);
+            Fraction frac = new(0, 5);
             frac.Inverse();
             frac.Evalue();
         }
