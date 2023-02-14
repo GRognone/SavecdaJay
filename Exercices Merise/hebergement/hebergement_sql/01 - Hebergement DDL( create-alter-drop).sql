24c6806b6b2c92bd143f4b207bb37183cae21879
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-02-14 12:29:02 +0100

    send 14 02 2023

--- Exercices Merise/hebergement/hebergement_sql/01 - Hebergement DDL( create-alter-drop).sql
+++ Exercices Merise/hebergement/hebergement_sql/01 - Hebergement DDL( create-alter-drop).sql
@@ -1,5 +1,6 @@
  -- use Hebergement;
-
+ DROP TABLE IF EXISTS Activites;
+ DROP TABLE IF EXISTS Type_Activites;
  DROP TABLE IF EXISTS Etats;
  DROP TABLE IF EXISTS Roles;
  DROP TABLE IF EXISTS Personnes;
@@ -26,4 +27,24 @@ PRIMARY KEY (role_id)
   etat_id INT IDENTITY (1,1),
   etat_intiltule VARCHAR(25),
   PRIMARY KEY (etat_id)
+  );
+
+  CREATE TABLE Type_Activites
+  (
+  type_activite_id INT IDENTITY (1,1),
+  type_activite_nom VARCHAR (50) NOT NULL UNIQUE (type_activite_nom) ,
+  PRIMARY KEY (type_activite_id)
+  );
+
+  CREATE TABLE Activites
+  (
+  activite_id INT IDENTITY (1,1),
+  activite_intitule VARCHAR(100) NOT NULL,
+  activite_debut DATETIME2 NOT NULL,
+  activite_fin DATETIME2 NOT NULL,
+  type_activite_id INT NOT NULL,
+  etat_id INT NOT NULL,
+  PRIMARY KEY (activite_id),
+  FOREIGN KEY (type_activite_id) REFERENCES Type_Activites (type_activite_id),
+  FOREIGN KEY (etat_id) REFERENCES Etats ( etat_id)
   );
