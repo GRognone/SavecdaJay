4fc360d50bf5dd307a86a8337593a2f8a886e349
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-02-16 15:55:48 +0100

    send 160223

---
+++ Exercices Merise/cars/Cars/DLL.sql
@@ -0,0 +1,40 @@
+-- use Cars
+DROP TABLE IF EXISTS Cars;
+DROP TABLE IF EXISTS Brands;
+DROP TABLE IF EXISTS Owners;
+
+
+CREATE TABLE Cars
+(
+	car_id INT NOT NULL,
+	car_registration CHAR(9) UNIQUE NOT NULL,
+	car_name VARCHAR (100) NOT NULL,
+	car_owner_id INT NOT NULL,
+	brand_id INT NOT NULL,
+	PRIMARY KEY (car_id)
+
+);
+
+CREATE TABLE Brands
+(
+	brand_id INT IDENTITY,
+	brand_name VARCHAR(50) NOT NULL,
+	brand_slogan VARCHAR (255) NULL,
+	PRIMARY KEY (brand_id)
+);
+
+CREATE TABLE Owners
+(
+	owner_id INT IDENTITY,
+	owner_lastname VARCHAR(50) NOT NULL,
+	owner_firstname VARCHAR(50) NOT NULL,
+	PRIMARY KEY (owner_id)
+);
+
+ALTER TABLE Cars
+	ADD CONSTRAINT FK_Cars_Owners FOREIGN KEY (car_owner_id) REFERENCES Owners(owner_id),
+	CONSTRAINT FK_Cars_Brands FOREIGN KEY (brand_id) REFERENCES Brands(brand_id)
+
+	SELECT * FROM Cars;
+	SELECT * FROM Brands;
+	SELECT * FROM Owners;
---
+++ Exercices Merise/cars/Cars/DML.sql
@@ -0,0 +1,19 @@
+--use Cars
+
+INSERT INTO Cars
+(car_id, car_registration, car_name)
+VALUES
+(12, 'AA-123-AA', 'Chevelle Concours'),
+(16, 'BB-274-BB', 'A6 Break'),
+(21, 'CA-789-BA', 'Corolla'),
+(19, 'CC-546-FV', 'Monte Carlo'),
+(27, 'CG-274-ZG', '504'),
+(30, 'ER-355-GX', 'Q8'),
+(28, 'FV-313-FT', '100 LS'),
+(23, 'DE-228-KS', 'Hornet'),
+(26, 'CF-614-PM', '3008');
+
+INSERT INTO Owners
+(owner_id, owner_lastname, owner_firstname)
+VALUES
+( ,'','')
