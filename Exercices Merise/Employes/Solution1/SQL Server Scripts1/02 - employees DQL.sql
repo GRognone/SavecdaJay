0855d699a885263ba5ba8d4226f3dd00d6fdff30
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-02-16 10:11:53 +0100

    send  16 02 2023 Morning

--- Exercices Merise/Employes/Solution1/SQL Server Scripts1/02 - employees DQL.sql
+++ Exercices Merise/Employes/Solution1/SQL Server Scripts1/02 - employees DQL.sql
@@ -33,7 +33,7 @@ E0.emp_hiredate,
 E0.emp_manager_id,
 E1.emp_id AS 'Manager id',
 E1.emp_lastname AS 'Manager last name',
-E1.emp_lastname AS 'Manager first name'
+E1.emp_firstname AS 'Manager first name'
 FROM employees AS E0
 LEFT JOIN employees AS E1 ON E0.emp_manager_id = E1.emp_id
 ORDER BY E1.emp_lastname ASC, E0.emp_hiredate DESC;
@@ -67,22 +67,32 @@ WHERE employees.emp_manager_id = 5;
 --6. Sélectionner le nom de tous les managers avec, pour chacun, la moyenne des salaires de leur subordonnés. Trier le résultat 
 --selon la valeur de la moyenne par ordre décroissant.
 
+-- solution avec une sous requette ( pas forcement la bonne solution)
 SELECT
 Man1.emp_lastname,
-AVG(Emp2.emp_SALARY) AS 'Salary_avarage',
-(SELECT AVG (Emp2.emp_salary) FROM employees Emp2 WHERE Emp2.emp_manager_id = Man1.emp_id AS 'Luc'
+AVG(Emp1.emp_salary) AS 'Average Salary',
+(SELECT AVG (Emp2.emp_SALARY) FROM employees Emp2 WHERE Emp2.emp_manager_id = Man1.emp_id ) AS 'tata'
 FROM employees AS Man1
 JOIN employees AS Emp1 ON Man1.emp_id = Emp1.emp_manager_id
 GROUP BY 
 Man1.emp_lastname,
+Man1.emp_id
 ORDER BY 
-Luc asc;
-
--- cette instruction vérifie s'il y a effectivement 5 employées qui son manadgé par le manager N°5
-SELECT COUNT(*) AS 'count subordinate'
-FROM employees
-WHERE employees.emp_manager_id = 5;
+'Average Salary' asc;
 
 
 --7. Créer la requête SELECT correspondant au résultat suivant :( voir document)
 
+SELECT
+Boss.emp_id 'Employee Id',
+Boss.emp_lastname AS 'Lastname',
+Boss.emp_firstname AS 'Firstname',
+Boss.emp_salary AS 'Salary',
+Boss.emp_hiredate AS 'Hiredate',
+COUNT(All_emp.emp_id) AS 'Number of Employees', -- comptabisile ne nombre d'employees
+SUM (All_emp.emp_salary) AS 'Total Salary',		-- totalise les salaires des employes
+AVG(All_emp.emp_salary) AS 'Average Salary'		-- calcul la moyenne des salaires des employes
+FROM employees AS Boss
+JOIN employees AS All_emp ON Boss.emp_id <> All_emp.emp_id
+WHERE Boss.emp_manager_id IS NULL AND All_emp.emp_manager_id IS NOT NULL
+GROUP BY Boss.emp_id, Boss.emp_lastname, Boss.emp_firstname, Boss.emp_salary, Boss.emp_hiredate
