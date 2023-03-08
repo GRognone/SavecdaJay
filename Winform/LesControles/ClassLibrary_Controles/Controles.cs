839c2dfe58a1804a04d737766791dd2a090d1f67
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-03-08 23:49:36 +0100

    send 08 03 23 night

--- Winform/LesControles/ClassLibrary_Controles/Controles.cs
+++ Winform/LesControles/ClassLibrary_Controles/Controles.cs
@@ -13,23 +13,25 @@ namespace ClassLibrary_Controles
 
 
 
-       public static bool Controle_saisie_nom(string nom_text)
+        public static bool Controle_saisie_nom(string nom_text)
         {
             System.Text.RegularExpressions.Regex valide_nom = new Regex(@"^[a-zA-Z]{0,30}$");
             return valide_nom.IsMatch(nom_text);
         }
-        /* 
-        public void Controle_saisie_date()
-        {
 
-        }
-        */
-
-        public void Controle_Saisie_Montant()
+        public static bool Controle_saisie_date(string date_text)
         {
-            System.Text.RegularExpressions.Regex valide_Saisie_Montant = new Regex (@"^$")
+            System.Text.RegularExpressions.Regex valide_date = new Regex(@"^([0-9]{0,2})$");
+            return valide_date.IsMatch(date_text);
         }
-     
+
+
+        public static bool Controle_Saisie_Montant(string montant_text)
+        {
+            System.Text.RegularExpressions.Regex valide_Saisie_Montant = new Regex(@"^([0-9]{0,12})([\,]{1})+([0-9]{0,2}$)");
+            return valide_Saisie_Montant.IsMatch(montant_text);
+        }
+
         public static bool Controle_Saisie_Code_Postal(string code_postal_text)
         {
             if (code_postal_text.Length == 5)
--- Winform/LesControles/test validite/Program.cs
+++ Winform/LesControles/test validite/Program.cs
@@ -3,7 +3,7 @@
 
 using ClassLibrary_Controles;
 using System.Text.RegularExpressions;
-
+/*
 bool ok1 = Controles.Controle_saisie_nom("AaZeRtGyUikojhuiguyguyk");
 if (ok1)
 {
@@ -13,9 +13,10 @@ else
 {
     Console.WriteLine("saisie incorrecte recommencez");
 }
+*/
 
-bool ok = Controles.Controle_Saisie_Code_Postal("15000");
-if (ok)
+
+bool ok2 = Controles.Controle_saisie_date("21/10/1977")
 {
     Console.WriteLine("c'est ok");
 }
@@ -24,6 +25,31 @@ else
     Console.WriteLine("saisie incorrecte recommencez");
 }
 
+
+/*
+bool ok3 = Controles.Controle_Saisie_Montant("125,22");
+
+    if (ok3)
+{
+    Console.WriteLine("c'est ok");
+}
+else
+{
+    Console.WriteLine("saisie incorrecte recommencez");
+}
+*/
+
+/*
+bool ok4 = Controles.Controle_Saisie_Code_Postal("15000");
+if (ok4)
+{
+    Console.WriteLine("c'est ok");
+}
+else
+{
+    Console.WriteLine("saisie incorrecte recommencez");
+}
+*/
 
 
 
