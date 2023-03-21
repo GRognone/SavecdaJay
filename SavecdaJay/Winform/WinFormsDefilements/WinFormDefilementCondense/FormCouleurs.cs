3c93e0d8b0c52b33a9bf775f999b9c5290d7f57c
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-03-21 14:56:15 +0100

    Send 21 03 2023 afternoon

--- SavecdaJay/Winform/WinFormsDefilements/WinFormDefilementCondense/FormCouleurs.cs
+++ SavecdaJay/Winform/WinFormsDefilements/WinFormDefilementCondense/FormCouleurs.cs
@@ -10,7 +10,6 @@ using System.Windows.Forms;
 
 namespace WinFormDefilementCondense
 {
-
     public partial class FormCouleurs : Form
     {
         System.Drawing.Color maCouleur;
@@ -23,55 +22,51 @@ namespace WinFormDefilementCondense
         private void MettreAjourIHM()
         {
             // 10 lignes de code max
+            this.hScrollBar_Red.Value = maCouleur.R;
             this.numericUpDown_Red.Value = maCouleur.R;
             panel_Red.BackColor = Color.FromArgb((int)maCouleur.R, 0, 0);
 
+            this.hScrollBar_Green.Value = maCouleur.G;
             this.numericUpDown_Green.Value = maCouleur.G;
             panel_Green.BackColor = Color.FromArgb(0, (int)maCouleur.G, 0);
 
+            this.hScrollBar_Blue.Value = maCouleur.B;
             this.numericUpDown_Blue.Value = maCouleur.B;
             panel_Blue.BackColor = Color.FromArgb(0, 0, (int)maCouleur.B);
 
             panel_ColorResult.BackColor = maCouleur;
         }
 
-        private void hScrollBar_Red_Scroll(object sender, ScrollEventArgs e)
+        private void hScrollBar_Red_ValueChanged(object sender, EventArgs e)
         {
-            maCouleur = Color.FromArgb((int)e.NewValue, maCouleur.G , maCouleur.B);
+            maCouleur = Color.FromArgb(hScrollBar_Red.Value, maCouleur.G, maCouleur.B);
             MettreAjourIHM();
         }
-
         private void numericUpDown_Red_ValueChanged(object sender, EventArgs e)
         {
-            hScrollBar_Red.Value = (int)numericUpDown_Red.Value;
-            panel_Red.BackColor = Color.FromArgb((int)numericUpDown_Red.Value, 0, 0);
-            panel_ColorResult.BackColor = Color.FromArgb((int)numericUpDown_Red.Value, (int)numericUpDown_Green.Value, (int)numericUpDown_Blue.Value);
-        }
-
-        private void hScrollBar_Green_Scroll(object sender, ScrollEventArgs e)
-        {
-            maCouleur = Color.FromArgb(maCouleur.R,(int)e.NewValue, maCouleur.B);
+            maCouleur = Color.FromArgb((int)numericUpDown_Red.Value, maCouleur.G, maCouleur.B);
             MettreAjourIHM();
         }
-
         private void numericUpDown_Green_ValueChanged(object sender, EventArgs e)
         {
-            hScrollBar_Green.Value = (int)numericUpDown_Green.Value;
-            panel_Green.BackColor = Color.FromArgb(0, (int)numericUpDown_Green.Value, 0);
-            panel_ColorResult.BackColor = Color.FromArgb((int)numericUpDown_Red.Value, (int)numericUpDown_Green.Value, (int)numericUpDown_Blue.Value);
-        }
-
-        private void hScrollBar_Blue_Scroll(object sender, ScrollEventArgs e)
-        {
-            maCouleur = Color.FromArgb(maCouleur.R, maCouleur.G, (int)e.NewValue);
+            maCouleur = Color.FromArgb(maCouleur.R, (int)numericUpDown_Green.Value, maCouleur.B);
             MettreAjourIHM();
         }
-
+        private void hScrollBar_Green_ValueChanged(object sender, EventArgs e)
+        {
+            maCouleur = Color.FromArgb(maCouleur.R, this.hScrollBar_Green.Value, maCouleur.B);
+            MettreAjourIHM();
+        }
+        private void hScrollBar_Blue_ValueChanged(object sender, EventArgs e)
+        {
+            maCouleur = Color.FromArgb(maCouleur.R, maCouleur.G, this.hScrollBar_Blue.Value);
+            MettreAjourIHM();
+        }
         private void numericUpDown_Blue_ValueChanged(object sender, EventArgs e)
         {
-            hScrollBar_Blue.Value = (int)numericUpDown_Blue.Value;
-            panel_Blue.BackColor = Color.FromArgb(0, 0, (int)numericUpDown_Blue.Value);
-            panel_ColorResult.BackColor = Color.FromArgb((int)numericUpDown_Red.Value, (int)numericUpDown_Green.Value, (int)numericUpDown_Blue.Value);
+            maCouleur = Color.FromArgb(maCouleur.R, maCouleur.G, (int)numericUpDown_Blue.Value);
+            MettreAjourIHM();
         }
+        
     }
 }
