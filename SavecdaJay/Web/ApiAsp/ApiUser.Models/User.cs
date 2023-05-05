e6e407c533f8c0573a7cd1a8af6cbf36da439d8a
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-05-05 11:52:58 +0200

    save 05 05 2023

---
+++ SavecdaJay/Web/ApiAsp/ApiUser.Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiUser.Models
+{
+    [Table("Users")] // sert à nommer la table.
+    public class User : Model
+    {
+        //[Column("user_name")] pour renommer une colonne
+        [Required(ErrorMessage = " Erreur de saisie")]
+        [MaxLength(16, ErrorMessage = "Erreur de saisie")]
+        [StringLength(16)]
+        public string? UserName { get ; set ; }
+       
+        public string? Password { get ; set ; }
+    }
+}
