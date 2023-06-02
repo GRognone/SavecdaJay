3eff7071db37abe12892ce7836c15539ff5a2cfd
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-06-02 12:54:29 +0200

    send 02 06 2023

---
+++ SavecdaJay/Web/exo-code-postaux/js/ZipCode.js
@@ -0,0 +1,51 @@
+ZipCodes()
+
+fetch('https://arfp.github.io/tp/web/frontend/zipcodes/zipcodes.json');
+.then(r=> r.json())
+.then(console.log())
+class ZipCode
+{
+    constructor()
+    {
+        this.datasource="https://arfp.github.io/tp/web/frontend/zipcodes/zipcodes.json";
+        this.zipcode = [];
+    }
+
+   async load(){
+    try{
+        let response = await fetch(this.datasource);
+        this.zipcodes = await response.json();
+        console.log(this.zipcodes)
+        console.log(this.zipcodes[0]);
+    }
+    catch(error){
+        //traitement de l'erreur
+    }
+   }
+
+   search(zipcode)
+   {
+    let result =[];
+   }
+}
+search(zipcode)
+{
+    let result = [];
+    result = this.zipcodes.filter(city => city.codePostal == zipcode);
+    console.log(result);
+    this.fillUi(result);
+}
+
+fillUi(result)
+{
+    // let element = document.getElementById('result');
+    let element = document.querySelector('result');
+    element.innerHTML = "";
+
+    for(let city of result)
+    {
+        let li = document.createElement('li');
+        li.innerText = city.nomCommune;
+        element.appendChild(li);
+    }
+}
