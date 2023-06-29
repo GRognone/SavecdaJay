ac46dc94595ea6e0a069e72345fd0e6121b9766d
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-06-29 12:57:29 +0200

    send noon acces 29 06 2023

---
+++ SavecdaJay/Accessibilité/exo-handyA11t/js/menu-deroulant.js
@@ -0,0 +1,18 @@
+document.querySelector('#boutonfold').addEventListener('click', e =>
+FaireDerouler(e.target,
+document.querySelector('#sectiontofold'),
+'Afficher le contenu de l\'image',
+'Masquer le contenu de l\'image'));
+
+function FaireDerouler(eventelement, foldelement, textWhenFold, textWhenUnfold){
+  
+    if (eventelement.dataset.fold == '0'){
+        foldelement.classList.add('fold');
+        eventelement.dataset.fold = '1';
+        eventelement.innerText = textWhenUnfold;
+    }  else if (eventelement.dataset.fold == '1'){
+        foldelement.classList.remove('fold');
+        eventelement.dataset.fold = '0';
+        eventelement.innerText = textWhenFold;
+    }
+}
