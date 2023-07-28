bab27b4c08536f5775d8260a32ab944591a982df
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-07-28 11:48:21 +0200

    send 28 07 2023

---
+++ SavecdaJay/Design Pattern/Design Pattern Iterator/test patterns/test Facade/test Facade/Program.cs
@@ -0,0 +1,127 @@
+﻿
+
+namespace Facade.RealWorld
+{
+    /// <summary>
+    /// Facade Design Pattern
+    /// </summary>
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Facade
+
+            Hypotheque Hypotheque = new Hypotheque();
+
+            // Evaluate Hypotheque eligibility for Client
+
+            Client client = new Client("Ann McKinsey");
+            bool eligible = Hypotheque.IsEligible(client, 125000);
+
+            Console.WriteLine("\n" + client.Name +
+                    " has been " + (eligible ? "Approved" : "Rejected"));
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subsystem ClassA' class
+    /// </summary>
+
+    public class Bank
+    {
+        public bool HasSufficientSavings(Client c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// The 'Subsystem ClassB' class
+    /// </summary>
+
+    public class Credit
+    {
+        public bool HasGoodCredit(Client c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// The 'Subsystem ClassC' class
+    /// </summary>
+
+    public class Loan
+    {
+        public bool HasNoBadLoans(Client c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Client class
+    /// </summary>
+
+    public class Client
+    {
+        private string name;
+
+        // Constructor
+
+        public Client(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+
+    /// <summary>
+    /// The 'Facade' class
+    /// </summary>
+
+    public class Hypotheque
+    {
+        Bank bank = new Bank();
+        Loan loan = new Loan();
+        Credit credit = new Credit();
+
+        public bool IsEligible(Client cust, int amount)
+        {
+            Console.WriteLine("{0} applies for {1:C} loan\n",
+                cust.Name, amount);
+
+            bool eligible = true;
+
+            // Check creditworthyness of applicant
+
+            if (!bank.HasSufficientSavings(cust, amount))
+            {
+                eligible = false;
+            }
+            else if (!loan.HasNoBadLoans(cust))
+            {
+                eligible = false;
+            }
+            else if (!credit.HasGoodCredit(cust))
+            {
+                eligible = false;
+            }
+
+            return eligible;
+        }
+    }
+}
+
