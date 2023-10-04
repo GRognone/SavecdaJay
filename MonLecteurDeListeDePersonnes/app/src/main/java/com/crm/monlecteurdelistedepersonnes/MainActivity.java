9664ebcd1054fbf3c8050c9a51bc875f88b326e6
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-10-04 16:56:32 +0200

    send 04 10 2023

--- MonLecteurDeListeDePersonnes/app/src/main/java/com/crm/monlecteurdelistedepersonnes/MainActivity.java
+++ MonLecteurDeListeDePersonnes/app/src/main/java/com/crm/monlecteurdelistedepersonnes/MainActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.util.DisplayMetrics;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.crm.classesdomaines.Personne;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+ListView myListOfPerson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +29,36 @@ public class MainActivity extends AppCompatActivity {
         personnes.add(new Personne("Gremlins", "Greta", this.getResources().getDrawableForDensity(R.drawable.furby3, DisplayMetrics.DENSITY_XHIGH)));
         personnes.add(new Personne("Schtroumpf", "Coquet"));
         personnes.add(new Personne("Dos Santos", "Manuel", this.getResources().getDrawableForDensity(R.drawable.chubaka,DisplayMetrics.DENSITY_XHIGH)));
-
-        ListView myListOfPerson = findViewById(R.id.ListviewMaListeDePersonne);
+        personnes.add(new Personne("Schtroumpf", "Coqt"));
+        myListOfPerson = findViewById(R.id.ListviewMaListeDePersonne);
         /*String[] tab = new String [] {"membre1", "membre2", "membre3", "membre4", "membre5"};
         ArrayAdapter arraydp = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tab);
         myListOfPerson.setAdapter(arraydp);*/
         myListOfPerson.setAdapter(new PersonneArrayAdaptater(this,personnes));
+        registerForContextMenu(myListOfPerson);
+        }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo);
 
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_list_personnes,menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        // recuperer des infos supplementaires quand il y a un click long sur un item
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
 
+        if (item.getItemId() == R.id.copie) {
+            Toast.makeText(getApplicationContext(), "Copie: ID " + menuInfo.id + " ,position" + menuInfo.position, Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.delete) {
+            Toast.makeText(getApplicationContext(), "Delete: ID " + menuInfo.id + " ,position" + menuInfo.position, Toast.LENGTH_SHORT).show();
+            PersonneArrayAdaptater paa = (PersonneArrayAdaptater) myListOfPerson.getAdapter();
+            Personne pASupprimer = paa.getItem(menuInfo.position);
+            paa.remove(pASupprimer);
+
+            // rafraichir la listeView pour voir les nouveaux elements.
+            paa.notifyDataSetChanged();
+        }return true;
     }
 }
