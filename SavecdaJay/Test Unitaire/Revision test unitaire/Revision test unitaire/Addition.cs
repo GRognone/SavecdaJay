753eb5a16416a3582d7a1700d3ea6a59cd1bd7ca
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-10-10 13:38:36 +0200

    send yesterday unit test

---
+++ SavecdaJay/Test Unitaire/Revision test unitaire/Revision test unitaire/Addition.cs
@@ -0,0 +1,22 @@
+﻿namespace Revision_test_unitaire
+{
+    public class Addition
+    {
+        private double nombre1;
+        private double nombre2;
+
+        public Addition(double n1, double n2)
+        {
+            this.nombre1 = n1;
+            this.nombre2 = n2;
+        }
+
+        public double Nombre2 { get => nombre2;  }
+        public double Nombre1 { get => nombre1; }
+
+        public double Resultat()
+        {
+            return nombre1 + nombre2;
+        }
+    }
+}
